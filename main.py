b71e6fd6501a0bd8d115b99d73e45660e220d065
Patrick Wang <patrick@covar.com>
2020-02-05 16:09:39 -0500

    add more io endpoints

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 """REST wrapper for KP registry SQLite server."""
 import sqlite3
-from typing import List
+from typing import Dict, List
 
-from fastapi import Depends, FastAPI, Query
+from fastapi import Body, Depends, FastAPI, Query
+from pydantic import AnyUrl, BaseModel
 
 app = FastAPI()
+
+
+class KP(BaseModel):
+    """Knowledge provider."""
+
+    source_type: str
+    edge_type: str
+    target_type: str
 
 
 def get_db():
@@ -16,14 +25,86 @@ def get_db():
         db.close()
 
 
-@app.get("/kp")
-def find_knowledge_providers(
+example = {
+    'my_kp_url': {
+        'source_type': 'disease',
+        'edge_type': 'related to',
+        'target_type': 'gene',
+    },
+}
+
+
+@app.get('/kps')
+def get_all_knowledge_providers(
+        db=Depends(get_db),
+):
+    """Get all knowledge providers."""
+    statement = 'SELECT * FROM knowledge_providers'
+    c = db.cursor()
+    c.execute(
+        statement,
+    )
+    results = c.fetchall()
+    return {
+        kp[0]: {
+            'source_type': kp[1],
+            'edge_type': kp[2],
+            'target_type': kp[3],
+        }
+        for kp in results
+    }
+
+
+@app.get('/kps/{url:path}')
+def get_knowledge_provider(
+        url: AnyUrl,
+        db=Depends(get_db),
+):
+    """Get a knowledge provider by url."""
+    statement = '''
+        SELECT source_type, edge_type, target_type FROM knowledge_providers
+        WHERE url=?
+        '''
+    c = db.cursor()
+    print(str(url))
+    c.execute(
+        statement,
+        (str(url),),
+    )
+    results = c.fetchone()
+    return {
+        'source_type': results[0],
+        'edge_type': results[1],
+        'target_type': results[2],
+    }
+
+
+@app.post('/kps')
+def add_knowledge_provider(
+        kps: Dict[str, KP] = Body(..., example=example),
+        db=Depends(get_db),
+):
+    """Add a knowledge provider."""
+    values = [
+        (url, kp.source_type, kp.edge_type, kp.target_type)
+        for url, kp in kps.items()
+    ]
+    with db:
+        # Create table
+        db.execute('''CREATE TABLE IF NOT EXISTS knowledge_providers
+                    (url text UNIQUE, source_type text, edge_type text, target_type text)''')
+        # Insert rows of data
+        db.executemany('INSERT INTO knowledge_providers VALUES (?, ?, ?, ?)', values)
+
+
+@app.get('/search')
+def search_for_knowledge_providers(
         source_type: List[str] = Query(..., example=['a']),
         edge_type: List[str] = Query(..., example=['related to']),
         target_type: List[str] = Query(..., example=['b']),
-        db=Depends(get_db)
+        db=Depends(get_db),
 ):
-    """Find a KP matching spec."""
+    """Search for knowledge providers matching a specification."""
     source_bindings = ', '.join('?' for _ in range(len(source_type)))
     edge_bindings = ', '.join('?' for _ in range(len(edge_type)))
     target_bindings = ', '.join('?' for _ in range(len(target_type)))
@@ -39,4 +120,5 @@ def find_knowledge_providers(
         list(source_type) + list(edge_type) + list(target_type)
     )
 
-    return [row[0] for row in c.fetchall()]
+    results = c.fetchall()
+    return [row[0] for row in results]
