232ca2bb1ef7f8bd47aad9a071c74d73fc783d12
Patrick Wang <patrick@covar.com>
2020-06-19 12:38:46 -0400

    Add package setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Setup file for kp-registry package."""
+from setuptools import setup
+
+setup(
+    name='kp_registry',
+    version='0.1.0-dev0',
+    author='Patrick Wang',
+    author_email='patrick@covar.com',
+    url='https://github.com/TranslatorIIPrototypes/kp_registry',
+    description='Translator KP Registry',
+    packages=['kp_registry'],
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT',
+    python_requires='>=3.6',
+)
