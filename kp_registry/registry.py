a4a38ec051331d12e97571f90731e1d03164eedd
Patrick Wang <patrick@covar.com>
2020-06-23 14:19:49 -0400

    Refactor to library-ize registry mechanics

---
+++ kp_registry/registry.py
@@ -0,0 +1,130 @@
+"""KP registry."""
+from collections import defaultdict
+import logging
+
+import aiosqlite
+from fastapi import HTTPException
+import sqlite3
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Registry():
+    """KP registry."""
+
+    def __init__(self, uri):
+        """Initialize."""
+        self.db = None
+        self.uri = uri
+
+    async def __aenter__(self):
+        """Enter context."""
+        self.db = await aiosqlite.connect(self.uri)
+        await self.setup()
+        return self
+
+    async def __aexit__(self, *args):
+        """Exit context."""
+        tmp_db = self.db
+        self.db = None
+        await tmp_db.close()
+
+    async def setup(self):
+        """Set up database table."""
+        await self.db.execute(
+            'CREATE TABLE IF NOT EXISTS knowledge_providers( ' + (
+                'url text, '
+                'source_type text, '
+                'edge_type text, '
+                'target_type text, '
+                'UNIQUE(url, source_type, edge_type, target_type) '
+            ) + ')'
+        )
+        await self.db.commit()
+
+    async def get_all(self):
+        """Get all KPs."""
+        statement = 'SELECT * FROM knowledge_providers'
+        cursor = await self.db.execute(
+            statement,
+        )
+        rows = await cursor.fetchall()
+        kps = defaultdict(list)
+        for row in rows:
+            kps[row[0]].append({
+                'source_type': row[1],
+                'edge_type': row[2],
+                'target_type': row[3],
+            })
+        return kps
+
+    async def get_one(self, url):
+        """Get a specific KP."""
+        statement = (
+            'SELECT source_type, edge_type, target_type '
+            'FROM knowledge_providers '
+            'WHERE url=?'
+        )
+        cursor = await self.db.execute(
+            statement,
+            (str(url),),
+        )
+        rows = await cursor.fetchall()
+        return [{
+            'source_type': row[0],
+            'edge_type': row[1],
+            'target_type': row[2],
+        } for row in rows]
+
+    async def add(self, kps):
+        """Add KP(s)."""
+        values = [
+            (url, kp.source_type, kp.edge_type, kp.target_type)
+            for url, kps in kps.items() for kp in kps
+        ]
+        # Insert rows of data
+        try:
+            await self.db.executemany(
+                'INSERT INTO knowledge_providers VALUES (?, ?, ?, ?)',
+                values
+            )
+        except sqlite3.IntegrityError as err:
+            if 'UNIQUE constraint failed' in str(err):
+                raise HTTPException(400, 'KP already exists')
+            raise err
+        await self.db.commit()
+
+    async def delete_one(self, url):
+        """Delete a specific KP."""
+        await self.db.execute(
+            'DELETE FROM knowledge_providers '
+            'WHERE url=?',
+            (url,),
+        )
+        await self.db.commit()
+
+    async def search(self, source_type, edge_type, target_type):
+        """Search for KPs matching a pattern."""
+        source_bindings = ', '.join('?' for _ in range(len(source_type)))
+        edge_bindings = ', '.join('?' for _ in range(len(edge_type)))
+        target_bindings = ', '.join('?' for _ in range(len(target_type)))
+        statement = (
+            'SELECT DISTINCT url FROM knowledge_providers '
+            f'WHERE source_type in ({source_bindings}) '
+            f'AND edge_type in ({edge_bindings}) '
+            f'AND target_type in ({target_bindings})'
+        )
+        cursor = await self.db.execute(
+            statement,
+            list(source_type) + list(edge_type) + list(target_type)
+        )
+
+        results = await cursor.fetchall()
+        return [row[0] for row in results]
+
+    async def delete_all(self):
+        """Delete all KPs."""
+        await self.db.execute(
+            'DELETE FROM knowledge_providers',
+        )
+        await self.db.commit()
--- kp_registry/routers/kps.py
+++ kp_registry/routers/kps.py
@@ -9,14 +9,15 @@ from pydantic import AnyUrl
 
 from ..models import KP
 from ..config import settings
+from ..registry import Registry
 
 router = APIRouter()
 
 
-async def get_db():
-    """Get SQLite connection."""
-    async with aiosqlite.connect(settings.db_uri) as db:
-        yield db
+async def get_registry():
+    """Get KP registry."""
+    async with Registry(settings.db_uri) as registry:
+        yield registry
 
 
 example = {
@@ -30,90 +31,37 @@ example = {
 
 @router.get('/kps')
 async def get_all_knowledge_providers(
-        db=Depends(get_db),
+        registry=Depends(get_registry),
 ):
     """Get all knowledge providers."""
-    statement = 'SELECT * FROM knowledge_providers'
-    cursor = await db.execute(
-        statement,
-    )
-    rows = await cursor.fetchall()
-    kps = defaultdict(list)
-    for row in rows:
-        kps[row[0]].append({
-            'source_type': row[1],
-            'edge_type': row[2],
-            'target_type': row[3],
-        })
-    return kps
+    return await registry.get_all()
 
 
 @router.get('/kps/{url:path}')
 async def get_knowledge_provider(
         url: AnyUrl,
-        db=Depends(get_db),
+        registry=Depends(get_registry),
 ):
     """Get a knowledge provider by url."""
-    statement = '''
-        SELECT source_type, edge_type, target_type FROM knowledge_providers
-        WHERE url=?
-        '''
-    cursor = await db.execute(
-        statement,
-        (str(url),),
-    )
-    rows = await cursor.fetchall()
-    return [{
-        'source_type': row[0],
-        'edge_type': row[1],
-        'target_type': row[2],
-    } for row in rows]
+    return await registry.get_one(url)
 
 
 @router.post('/kps', status_code=status.HTTP_201_CREATED)
 async def add_knowledge_provider(
         kps: Dict[AnyUrl, List[KP]] = Body(..., example=example),
-        db=Depends(get_db),
+        registry=Depends(get_registry),
 ):
     """Add a knowledge provider."""
-    values = [
-        (url, kp.source_type, kp.edge_type, kp.target_type)
-        for url, kps in kps.items() for kp in kps
-    ]
-    # Create table
-    await db.execute('''
-    CREATE TABLE IF NOT EXISTS knowledge_providers(
-        url text,
-        source_type text,
-        edge_type text,
-        target_type text,
-        UNIQUE(url, source_type, edge_type, target_type)
-    )''')
-    # Insert rows of data
-    try:
-        await db.executemany(
-            'INSERT INTO knowledge_providers VALUES (?, ?, ?, ?)',
-            values
-        )
-    except sqlite3.IntegrityError as err:
-        if 'UNIQUE constraint failed' in str(err):
-            raise HTTPException(400, 'KP already exists')
-        raise err
-    await db.commit()
+    await registry.add(kps)
 
 
 @router.delete('/kps/{url:path}', status_code=status.HTTP_204_NO_CONTENT)
 async def remove_knowledge_provider(
         url: AnyUrl,
-        db=Depends(get_db),
+        registry=Depends(get_registry),
 ):
     """Delete a knowledge provider."""
-    await db.execute(
-        '''DELETE FROM knowledge_providers
-            WHERE url=?''',
-        (url,),
-    )
-    await db.commit()
+    await registry.delete_one(url)
 
 
 @router.post('/search')
@@ -121,33 +69,15 @@ async def search_for_knowledge_providers(
         source_type: List[str] = Body(..., example=['drug']),
         edge_type: List[str] = Body(..., example=['-related_to->']),
         target_type: List[str] = Body(..., example=['named_thing']),
-        db=Depends(get_db),
+        registry=Depends(get_registry),
 ):
     """Search for knowledge providers matching a specification."""
-    source_bindings = ', '.join('?' for _ in range(len(source_type)))
-    edge_bindings = ', '.join('?' for _ in range(len(edge_type)))
-    target_bindings = ', '.join('?' for _ in range(len(target_type)))
-    statement = f'''
-        SELECT DISTINCT url FROM knowledge_providers
-        WHERE source_type in ({source_bindings})
-        AND edge_type in ({edge_bindings})
-        AND target_type in ({target_bindings})
-        '''
-    cursor = await db.execute(
-        statement,
-        list(source_type) + list(edge_type) + list(target_type)
-    )
-
-    results = await cursor.fetchall()
-    return [row[0] for row in results]
+    return await registry.search(source_type, edge_type, target_type)
 
 
 @router.post('/clear', status_code=status.HTTP_204_NO_CONTENT)
 async def clear_kps(
-    db=Depends(get_db),
+    registry=Depends(get_registry),
 ):
     """Clear all registered KPs."""
-    await db.execute(
-        '''DELETE FROM knowledge_providers''',
-    )
-    await db.commit()
+    await registry.delete_all()
