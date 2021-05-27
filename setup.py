d1979172e409ff56c82ed306a81ce9fea0102cde
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-05-27 11:07:50 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='kp_registry',
-    version='2.0.0',
+    version='2.1.0',
     author='Patrick Wang',
     author_email='patrick@covar.com',
     url='https://github.com/TranslatorIIPrototypes/kp_registry',
