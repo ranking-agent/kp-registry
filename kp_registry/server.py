7819cc2f8e12ffd4cbaf9eabe08f815f1af27f0b
Abrar Mesbah <abrar.mesbah@gmail.com>
2022-02-04 13:50:38 -0500

    bump version

--- kp_registry/server.py
+++ kp_registry/server.py
@@ -6,7 +6,7 @@ from .routers.kps import registry_router, load_from_smartapi, register_endpoints
 openapi_args = dict(
     title="Knowledge Provider Registry",
     description="Registry of Translator knowledge providers",
-    version="2.4.0",
+    version="2.4.1",
     translator_teams=["Ranking Agent"],
 )
 
