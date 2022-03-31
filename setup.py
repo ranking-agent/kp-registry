5e8ed62a64646f8b74499312a19f5ccbb6cac8df
Max Wang <max@covar.com>
2022-03-31 17:25:24 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup
 
 setup(
     name='kp_registry',
-    version='2.4.0',
-    author='Patrick Wang',
-    author_email='patrick@covar.com',
-    url='https://github.com/TranslatorIIPrototypes/kp_registry',
+    version='2.4.2',
+    author='CoVar',
+    url='https://github.com/ranking-agent/kp_registry',
     description='Translator KP Registry',
     packages=['kp_registry', 'kp_registry.routers'],
     include_package_data=True,
