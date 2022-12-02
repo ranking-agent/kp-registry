524049db4420e27b83aff4fc1d959f87e87a3e1d
Max Wang <max@covar.com>
2022-12-02 15:07:08 -0500

    Run black on all files

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from setuptools import setup
 
 setup(
-    name='kp-registry',
-    version='2.6.0',
-    author='CoVar',
-    url='https://github.com/ranking-agent/kp_registry',
-    description='Translator KP Registry',
-    packages=['kp_registry'],
+    name="kp-registry",
+    version="2.6.0",
+    author="CoVar",
+    url="https://github.com/ranking-agent/kp_registry",
+    description="Translator KP Registry",
+    packages=["kp_registry"],
     include_package_data=True,
     zip_safe=False,
-    license='MIT',
-    python_requires='>=3.6',
+    license="MIT",
+    python_requires=">=3.6",
     install_requires=[
         "httpx",
         "reasoner_pydantic",
